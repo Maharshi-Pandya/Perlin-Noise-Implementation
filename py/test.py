71d026ea74dd5cbcadb33d6e0a1ff8219eb5207d
Maharshi-Pandya <maharshi.pandya2002@gmail.com>
2020-10-14 19:57:04 +0530

    :sparkles: Can also generate noise using octaves contribution

--- py/test.py
+++ py/test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3.9
 # -*- coding: utf-8 -*-
 import PerlinNoise
+
+# the perlin noise instance
 noise = PerlinNoise.PerlinNoise(3.14, 42.0 , 7.0)
+
+# noise.val() returns the calculated noise value at the input point
 print(noise.val())
